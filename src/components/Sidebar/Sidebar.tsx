54640cad2c0f37992bc7dbd99632fd628a784c34
Wyctus <68960527+Imuqaddas@users.noreply.github.com>
2022-04-30 02:33:17 +0200

    feat: created Layout component

---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,112 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import Toolbar from "@mui/material/Toolbar";
+import Divider from "@mui/material/Divider";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
+import ApartmentIcon from "@mui/icons-material/Apartment";
+import MeetingRoomIcon from "@mui/icons-material/MeetingRoom";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import CorporateFareIcon from "@mui/icons-material/CorporateFare";
+import SettingsIcon from "@mui/icons-material/Settings";
+import KeyIcon from "@mui/icons-material/Key";
+import EventAvailableIcon from "@mui/icons-material/EventAvailable";
+import Face from "@mui/icons-material/Face";
+
+import { Link as RouterLink } from "react-router-dom";
+import Link from "@mui/material/Link";
+
+function Sidebar() {
+  return (
+    <div>
+      <Toolbar>
+        <img
+          src="/images/test-company1.png"
+          css={css`
+            height: 65px;
+          `}
+          alt="logo"
+        ></img>
+      </Toolbar>
+      <Divider />
+      <List>
+        <Link component={RouterLink} to="/">
+          <ListItem button key={"Dashboard"}>
+            <ListItemIcon>
+              <DashboardIcon />
+            </ListItemIcon>
+            <ListItemText primary="Dashboard" />
+          </ListItem>
+        </Link>
+      </List>
+      <Divider />
+      <List>
+        <Link component={RouterLink} to="/buildings">
+          <ListItem button key={"Buildings"}>
+            <ListItemIcon>
+              <ApartmentIcon />
+            </ListItemIcon>
+            <ListItemText primary="Buildings" />
+          </ListItem>
+        </Link>
+        <Link component={RouterLink} to="/offices">
+          <ListItem button key={"Offices"}>
+            <ListItemIcon>
+              <MeetingRoomIcon />
+            </ListItemIcon>
+            <ListItemText primary="Offices" />
+          </ListItem>
+        </Link>
+        <Link component={RouterLink} to="/rentings">
+          <ListItem button key={"Rentings"}>
+            <ListItemIcon>
+              <KeyIcon />
+            </ListItemIcon>
+            <ListItemText primary="Rentings" />
+          </ListItem>
+        </Link>
+        <ListItem button key={"Bookings"}>
+          <ListItemIcon>
+            <EventAvailableIcon />
+          </ListItemIcon>
+          <ListItemText primary="Bookings" />
+        </ListItem>
+        <ListItem button key={"Users"}>
+          <ListItemIcon>
+            <Face />
+          </ListItemIcon>
+          <ListItemText primary="Users" />
+        </ListItem>
+      </List>
+      <Divider />
+      <List>
+        <ListItem button key={"Finance"}>
+          <ListItemIcon>
+            <AttachMoneyIcon />
+          </ListItemIcon>
+          <ListItemText primary="Finance" />
+        </ListItem>
+        <ListItem button key={"Company"}>
+          <ListItemIcon>
+            <CorporateFareIcon />
+          </ListItemIcon>
+          <ListItemText primary="Company" />
+        </ListItem>
+      </List>
+      <Divider />
+      <List>
+        <ListItem button key={"Settings"}>
+          <ListItemIcon>
+            <SettingsIcon />
+          </ListItemIcon>
+          <ListItemText primary="Settings" />
+        </ListItem>
+      </List>
+    </div>
+  );
+}
+
+export default Sidebar;
