0255095bae676a73ea3969e9854d1425cea080fd
Imugaddas <68960527+Imuqaddas@users.noreply.github.com>
2022-05-03 12:33:19 +0200

    feat: redirected unauthenticated users

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import useAuth, { Auth } from "./hooks/useAuth";
 
 import { Buildings } from "./pages/Buildings";
 import CreateBuilding from "./pages/CreateBuilding/CreateBuilding";
@@ -6,30 +7,38 @@ import CreateOffice from "./pages/CreateOffice/CreateOffice";
 import CreateRenting from "./pages/CreateRenting/CreateRenting";
 import CreateRentingWithId from "./pages/CreateRentingWithId/CreateRentingWithId";
 import { Dashboard } from "./pages/Dashboard";
+import Login from "./pages/Login/Login";
 import OfficeData from "./pages/OfficeData/OfficeData";
 import { Offices } from "./pages/Offices";
 import RentingData from "./pages/RentingData/RentingData";
 import Rentings from "./pages/Rentings/Rentings";
 
 function App() {
+  const user = useAuth();
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/buildings" element={<Buildings />} />
-        <Route path="/offices" element={<Offices />} />
-        <Route path="/offices/create" element={<CreateOffice />} />
-        <Route path="/office/:id" element={<OfficeData />} />
-        <Route path="/rentings" element={<Rentings />} />
-        <Route path="/renting/:id" element={<RentingData />} />
+        <Route path="/" element={<ProtectedScreen element={<Dashboard />} user={user} />} />
+        <Route path="/buildings" element={<ProtectedScreen element={<Buildings />} user={user} />} />
+        <Route path="/offices" element={<ProtectedScreen element={<Offices />} user={user} />} />
+        <Route path="/offices/create" element={<ProtectedScreen element={<CreateOffice />} user={user} />} />
+        <Route path="/office/:id" element={<ProtectedScreen element={<OfficeData />} user={user} />} />
+        <Route path="/rentings" element={<ProtectedScreen element={<Rentings />} user={user} />} />
+        <Route path="/renting/:id" element={<ProtectedScreen element={<RentingData />} user={user} />} />
         <Route path="/rentings/create">
-          <Route path="/rentings/create/:officeId" element={<CreateRentingWithId />} />
-          <Route path="" element={<CreateRenting />} />
+          <Route path="/rentings/create/:buildingId/:officeId" element={<ProtectedScreen element={<CreateRentingWithId />} user={user} />} />
+          <Route path="" element={<ProtectedScreen element={<CreateRenting />} user={user} />} />
         </Route>
-        <Route path="/buildings/create" element={<CreateBuilding />} />
+        <Route path="/buildings/create" element={<ProtectedScreen element={<CreateBuilding />} user={user} />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
+}
+
+function ProtectedScreen(props: { element: JSX.Element; user?: Auth }) {
+  return props.user ? props.element : <Navigate to="/login" replace />;
 }
 
 export default App;
